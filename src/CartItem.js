e33d8755093ab3b274c9bf5d9b502e7a541ae55b
Vaibhav <vaibhav10127jain@gmail.com>
2021-12-20 18:10:07 +0530

    Added all buttons functionality

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,19 +1,13 @@
 import React from 'react';
 class CartItems extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            title: "Mobile Phone",
-            price: 999,
-            qty: 1,
-            img: ''
-        }
-    }
-    increaseQuantity = () => {
-        console.log(this.state);
-    }
     render() {
-        const { title, price, qty } = this.state;
+        const { title, price, qty } = this.props.product;
+        const {
+            product, 
+            OnclickIncrease, 
+            OnclickDecrease, 
+            OnDeleteQuantity
+        } = this.props;
         return (
             <div className="cart-item">
                 <div className="left-block">
@@ -29,17 +23,19 @@ class CartItems extends React.Component {
                             alt="increase" 
                             className="action-icons" 
                             src="https://cdn-icons.flaticon.com/png/512/3303/premium/3303893.png?token=exp=1639925965~hmac=805c4a19abf2a1993f81ce43731cf5d1"
-                            onClick={this.increaseQuantity}
+                            onClick={() => {OnclickIncrease(product)}}
                         />
                         <img
                             alt="decrease" 
                             className="action-icons" 
                             src="https://cdn-icons.flaticon.com/png/512/2740/premium/2740679.png?token=exp=1639926031~hmac=a81c8424b31ff2e189d8a7356b251687"
+                            onClick={() => {OnclickDecrease(product)}}
                         />
                         <img 
                             alt="delete" 
                             className="action-icons" 
                             src="https://cdn-icons-png.flaticon.com/512/1214/1214428.png"
+                            onClick={() => {OnDeleteQuantity(product.id)}}
                         />
                     </div>
                 </div>
